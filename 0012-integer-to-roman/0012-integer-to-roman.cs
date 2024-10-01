62e17d3c6d3b641dcf7b403684cd8145eb9e5675
ramyak457 <162635691+ramyak457@users.noreply.github.com>
2024-09-30 23:59:27 -0700

    Time: 54 ms (77.47%), Space: 48.5 MB (17.05%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text;
+public class Solution {
+    public string IntToRoman(int num) {
+        var dict = new Dictionary<int,string>
+        {{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},
+        {10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
+        var romanBuilder = new StringBuilder();
+        foreach(var romanInt in dict.Keys)
+        {
+            while(romanInt<=num)
+            {
+                romanBuilder.Append(dict[romanInt]);
+                num -=romanInt;
+            }      
+        }
+        var romanNumber = romanBuilder.ToString();
+        return romanNumber;
+    }
+}
