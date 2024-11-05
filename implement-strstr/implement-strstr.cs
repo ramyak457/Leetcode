765b478588ebd768f68bb53ff467ac18e25b2255
ramyak457 <162635691+ramyak457@users.noreply.github.com>
2024-11-05 12:07:33 -0800

    Time: 8 ms, Memory: 39.4 MB - LeetHub

---
+++ implement-strstr/implement-strstr.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        if(!haystack.Contains(needle))
+        {
+            return -1;
+        }
+        return haystack.IndexOf(needle);
+    }
+}
